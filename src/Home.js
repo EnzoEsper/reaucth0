17e6939ecc1d16706ee8564fb760b0ecff6740d3
enzoesper <enzoesper@hotmail.com>
2020-11-30 01:51:10 -0300

    checking if the user is authenticated

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
+    // destructuring props
+    const { isAuthenticated, login } = this.props.auth;
     return(
       <div>
         <h1>Home</h1>
-        {/* this login method is the one that is declared in Auth.js */}
-        <button onClick={this.props.auth.login}>Login</button>
+        { isAuthenticated() ?
+          <Link to="/profile">View Profile</Link>
+          /* this login method is the one that is declared in Auth.js */
+          : <button onClick={login}>Login</button>
+        }
       </div>
     );
   }
