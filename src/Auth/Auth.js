10b6482577e607103771c972d939e80c938ae0a5
enzoesper <enzoesper@hotmail.com>
2020-11-30 01:39:07 -0300

    implementing the handleAuthentication function

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -18,4 +18,32 @@ export default class Auth {
     // tho the auth0 login page
     this.auth0.authorize();
   };
+
+  handleAuthentication = () => {
+    // function that parses the hash from the url
+    this.auth0.parseHash((err, authResult) => {
+      // if we receive an authResult and have an acces and id token
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        // we create the session and store some data in local storage
+        this.setSession(authResult);
+        // programatically redirects the app to the home page
+        this.history.push("/");
+      } else if (err) {
+        // handling errors
+        this.history.push("/");
+        alert(`Error: ${err.error}. Check the console for more details`);
+        console.log(err);
+      }
+    });
+  };
+
+  setSession = (authResult) => {
+    // set the time that the acces token will expire. this gives us the Unix epoch time when the token will expire.
+    const expiresAt = JSON.stringify(authResult.expiresIn * 1000 + new Date().getTime());
+
+    // store some data in local storage
+    localStorage.setItem("access_token", authResult.accessToken);
+    localStorage.setItem("id_token", authResult.idToken);
+    localStorage.setItem("expires_at", expiresAt);
+  };
 }
